bee954f12bb89d598349adf021b49325c0b99ca7
Ravi0529 <150603739+Ravi0529@users.noreply.github.com>
2025-04-12 01:33:21 +0530

    deployed api added #2

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -18,7 +18,7 @@ function App() {
     setInput("");
 
     try {
-      const res = await fetch("http://127.0.0.1:8000/chat", {
+      const res = await fetch("https://persona-chatbot.onrender.com/chat", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
